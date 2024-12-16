a5d8215bf04e27ef326480a716b193aa6c792496
Dilanrmv <114661778+Dilanrmv@users.noreply.github.com>
2024-12-16 16:15:59 +0000

    third issue

--- FileUpload/fileupload1.php
+++ FileUpload/fileupload1.php
@@ -22,10 +22,11 @@
 // Check if image file is a actual image or fake image
 if(isset($_POST["submit"])) {
 	$target_dir = "uploads/";
-	$target_file = $target_dir . basename($_FILES["file"]["name"]);
+	$target_file = $target_dir . uniqid('', true) . '-' . basename($_FILES["file"]["name"]);
 	
     move_uploaded_file($_FILES["file"]["tmp_name"], $target_file);
-    echo "File uploaded /uploads/".$_FILES["file"]["name"];
+    echo "File uploaded /uploads/" . htmlspecialchars(basename($_FILES["file"]["name"]));
+
 }
 ?>
 </body>
